999e3824356c5f8ba7d9fa1417f1b3c502afb207
Joe Booth <joseph.booth3@gmail.com>
2018-04-06 18:52:08 -0500

    JS file complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 var wins = 0;
 var losses = 0;
 var random_number;
+var your_number = 0;
 var crystal_values = [];
 
 //generate random number between 19 and 120 to be targeted
@@ -30,6 +31,7 @@ function crystalValues() {
 function reset() {
     var wins = 0;
     var losses = 0;
+    your_number = 0;
     var random_number = Math.floor(Math.random() * 100) +19;
     var crystal_values = [];
     crystalValues();
@@ -49,7 +51,61 @@ function lose() {
     reset();
 }
 
+//setting click event for blue crystal
 
+$("#blue").on('click', function(){
+    your_number = your_number + crystal_values[0];
+    console.log("New total = " + your_number);
+    $("#total").text(your_number);
+
+        if (your_number === random_number) {
+            win();
+        } else if (your_number > random_number) {
+            lose();
+        }
+})
+
+//setting click event for green crystal
+
+$("#green").on('click', function(){
+    your_number = your_number + crystal_values[1];
+    console.log("New total = " + your_number);
+    $("#total").text(your_number);
+
+        if (your_number === random_number) {
+            win();
+        } else if (your_number > random_number) {
+            lose();
+        }
+})
+
+//setting click event for yellow crystal
+
+$("#yellow").on('click', function(){
+    your_number = your_number + crystal_values[2];
+    console.log("New total = " + your_number);
+    $("#total").text(your_number);
+
+        if (your_number === random_number) {
+            win();
+        } else if (your_number > random_number) {
+            lose();
+        }
+})
+
+//setting click event for red crystal
+
+$("#red").on('click', function(){
+    your_number = your_number + crystal_values[3];
+    console.log("New total = " + your_number);
+    $("#total").text(your_number);
+
+        if (your_number === random_number) {
+            win();
+        } else if (your_number > random_number) {
+            lose();
+        }
+})
 
 });
 
